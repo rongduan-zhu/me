9df8ee06cfd2ee97f5c9cf12d88073118ab6b74c
Rongduan Zhu <rongduan.zhu@gmail.com>
2016-03-16 23:27:05 -0700

    added css loaders and linter

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,26 @@
 
 const path = require('path');
 
+const autoprefixer = require('autoprefixer')
+const precss = require('precss')
+
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
+let extractCss = new ExtractTextPlugin('css/[name].css');
+
 module.exports = {
   context: path.resolve(__dirname, 'input'),
   entry: {
-    app: './js/app.js'
+    app: './js/app.js',
+    style: './css/app.css'
   },
   output: {
     path: path.resolve(__dirname, 'output'),
     filename: 'js/[name].js'
   },
   module: {
+    preLoaders: [
+      { test: /\.css$/, loader: 'stylelint' }
+    ],
     loaders: [
       {
         test: /\.js$/,
@@ -23,8 +33,18 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: 'style!css'
+        loader: extractCss.extract('style', 'css!postcss')
       }
     ]
+  },
+  plugins: [extractCss],
+  postcss: function() {
+    return [autoprefixer, precss];
+  },
+  stylelint: {
+    configFile: path.join(__dirname, './.stylelintrc')
+  },
+  watchOptions: {
+    poll: true
   }
 }
