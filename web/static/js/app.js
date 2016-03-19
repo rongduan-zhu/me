e512c0c3b31dba7255fa3f8ccdea98acd520b6dd
Rongduan Zhu <rongduan.zhu@gmail.com>
2016-03-19 00:39:31 -0700

    inited angular app

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,8 +1,13 @@
-require('./socket');
+require('./socket.js');
 
 import "phoenix_html";
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
+angular.module('app', [])
+.component('app', {
+  template: '<div>Rongduan said hi {{ $ctrl.secondsPast }} seconds ago.</div>',
+  controller($interval) {
+    this.secondsPast = 0;
+
+    $interval(() => this.secondsPast++, 1000);
+  }
+});
