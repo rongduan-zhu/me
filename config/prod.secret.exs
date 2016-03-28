dd57dc149a34932a4643006fbdd0d65586cda370
Rongduan Zhu <rongduan.zhu@gmail.com>
2016-03-28 00:49:26 -0700

    removed db duplicate defination

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -5,11 +5,3 @@ use Mix.Config
 # your version control system.
 config :me, Me.Endpoint,
   secret_key_base: "aak7GdQsSJpHb/vNMzpnKF3bs+2PMf643e4Ujrd6TRgQ2N5QfdZKRGzL9XUS+izU"
-
-# Configure your database
-config :me, Me.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "me_prod",
-  pool_size: 20
