218dadb61d6caa9a3426d5f0675e6884760f1da2
Rongduan Zhu <rongduan.zhu@gmail.com>
2016-04-03 16:53:49 -0700

    more robust environment checking

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,13 +8,37 @@ set :pty, true
 
 dc = ['docker-compose', '-f', 'docker-compose.yml', '-f', 'docker-compose.prod.yml']
 
+def execute_sudo *args
+  execute :sudo, '-E', *args
+end
+
 namespace :deploy do
   desc 'Normal code update'
   task :code do
     on roles(:app) do
       within('/var/me') do
-        execute :sudo, :git, :pull
-        execute :sudo, *dc, :restart
+        execute_sudo :git, :pull
+        execute_sudo *dc, :restart
+      end
+    end
+  end
+
+  desc 'Image update'
+  task :image do
+    on roles(:app) do
+      within('/var/me') do
+        execute_sudo :git, :pull
+        execute_sudo './bin/bootstrap'
+        execute_sudo *dc, :up, '-d'
+      end
+    end
+  end
+
+  desc 'Start container'
+  task :up do
+    on roles(:app) do
+      within('/var/me') do
+        execute_sudo *dc, :up, '-d'
       end
     end
   end
